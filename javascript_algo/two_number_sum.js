7cf48acbe07691945ddd26069351fff3f08ccaec
AndyWams <andrew.wams@gmail.com>
2022-07-09 07:31:23 +0100

    added descriptive message

--- javascript_algo/two_number_sum.js
+++ javascript_algo/two_number_sum.js
@@ -1,3 +1,22 @@
+/* 
+  Write a function that takes in a non-empty array of distinct integers and an
+  integer representing a target sum. If any two numbers in the input array sum
+  up to the target sum, the function should return them in an array, in any
+  order. If no two numbers sum up to the target sum, the function should return
+  an empty array.
+
+  Note that the target sum has to be obtained by summing two different integers
+  in the array; you can't add a single integer to itself in order to obtain the
+  target sum.
+
+  You can assume that there will be at most one pair of numbers summing up to
+  the target sum.
+
+*/
+
+//sample input: [3, 5, -4, 8, 11, 1, -1, 6]
+//samplet target: 10
+
 //solution one
 function twoNumberSum(array, targetSum) {
   for (let i = 0; i < array.length; i++) {
@@ -9,6 +28,7 @@ function twoNumberSum(array, targetSum) {
   }
   return [];
 }
+
 //Solution two
 function twoNumberSum(array, targetSum) {
   // Write your code here.
